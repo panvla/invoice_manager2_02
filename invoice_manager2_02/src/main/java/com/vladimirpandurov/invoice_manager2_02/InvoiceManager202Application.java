357fb559fc34ac92d923c3d554cf8051e1fc470a
panvla <pandvlado@gmail.com>
2024-02-03 13:57:50 +0100

    added initial project

---
+++ invoice_manager2_02/src/main/java/com/vladimirpandurov/invoice_manager2_02/InvoiceManager202Application.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.invoice_manager2_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceManager202Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceManager202Application.class, args);
+	}
+
+}
---
+++ invoice_manager2_02/src/test/java/com/vladimirpandurov/invoice_manager2_02/InvoiceManager202ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladimirpandurov.invoice_manager2_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManager202ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
